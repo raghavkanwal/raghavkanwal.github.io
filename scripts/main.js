eb0d88cef3a0241695aa29c64ff71aa3c6164482
Raghav Kanwal <raghav.kanwal@gmail.com>
2017-07-18 17:17:00 +0530

    Changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,4 @@
 jQuery(document).ready(function() {
-console.log("yooyyoo");
 	jQuery("button").on('click', function() {
 		jQuery(".intro").fadeOut(1500).promise().done( function() {
 			jQuery(".soon").fadeIn(1500).css('display','table-cell');
